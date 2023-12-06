85eda9b5aa3cb9b0e8e6946fe7eaf0c743c28aa7
tatenda <tatendazimba@gmail.com>
2023-12-06 09:37:45 +0200

    feat: add Google books remote assets config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'books.google.com',
+                port: '',
+                pathname: '/books/content',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
