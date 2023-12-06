067e89f496667b01ea8d77b453ac823fe9060d12
tatenda <tatendazimba@gmail.com>
2023-12-06 09:52:13 +0200

    feat: add vertical image card

---
+++ src/components/shared/ImageCardVertical/index.tsx
@@ -0,0 +1,47 @@
+import { AspectRatio, Box, Card, CardContent, Typography } from "@mui/joy";
+import Image from "next/image";
+import placeholderImage from "@/assets/images/placeholder.jpg";
+
+type Props = {
+    title: string,
+    description: string,
+    imageUrl?: string,
+};
+
+export const ImageCardVertical: React.FC<Props> = props => {
+    const { title, description, imageUrl } = props;
+
+    return (
+        <Card>
+            <AspectRatio
+                ratio={9/16}
+            >
+                <Image
+                    fill
+                    src={imageUrl || placeholderImage}
+                    loading="lazy"
+                    alt={title}
+                    style={{
+                        objectFit: 'contain',
+                    }}
+                />
+            </AspectRatio>
+
+            <CardContent>
+                <Box>
+                    <Typography
+                        level="title-lg"
+                    >
+                        {title}
+                    </Typography>
+
+                    <Typography
+                        level="body-sm"
+                    >
+                        {description}
+                    </Typography>
+                </Box>
+            </CardContent>
+        </Card>
+    )
+}
