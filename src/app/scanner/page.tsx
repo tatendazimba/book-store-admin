ae7fc23ebc9cdb01235ce3683240b1d52e2191b0
tatenda <tatendazimba@gmail.com>
2023-12-06 09:47:35 +0200

    feat: add scanner page

---
+++ src/app/scanner/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { ScanditScanner } from "@/components/shared/Scanner";
+
+export default function Scanner() {
+    return (
+        <main>
+            <ScanditScanner />
+        </main>
+    )
+}
