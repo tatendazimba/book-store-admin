cf6040554861ab5e68b673a955b45db4042881ba
tatenda <tatendazimba@gmail.com>
2023-12-07 11:38:48 +0200

    feat: search for book using scanned barcode

--- src/app/scanner/page.tsx
+++ src/app/scanner/page.tsx
@@ -1,11 +1,34 @@
 "use client";
 
 import { ScanditScanner } from "@/components/shared/Scanner";
+import { Box } from "@mui/joy";
+
+import styles from './styles.module.scss';
+import { SearchContainer } from "@/components/shared/SearchContainer";
+import { Search } from "@/components/shared/Search";
+import { useState } from "react";
 
 export default function Scanner() {
+    const [barcode, setBarcode] = useState('');
+
+    const onFound = (barcode: string) => {
+        setBarcode(barcode);
+    };
+
     return (
         <main>
-            <ScanditScanner />
+            <Box>
+                <ScanditScanner
+                    onFound={onFound}
+                />
+                <Box
+                    className={styles.search_container}
+                >
+                    <SearchContainer searchValue={barcode}>
+                        <Search value={barcode} key={barcode}/>
+                    </SearchContainer>
+                </Box>
+            </Box>
         </main>
     )
 }
