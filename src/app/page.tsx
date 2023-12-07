f5322a2a50b47d52f80c9d3f66791d38b0a636fb
tatenda <tatendazimba@gmail.com>
2023-12-07 11:39:13 +0200

    fix: update search components hierarchy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,15 @@
 import { Container } from '@mui/joy';
 import styles from './page.module.css';
 import { SearchContainer } from '@/components/shared/SearchContainer';
+import { Search } from '@/components/shared/Search';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Container>
-        <SearchContainer
-        />
+        <SearchContainer>
+          <Search />
+        </SearchContainer>
       </Container>
     </main>
   )
